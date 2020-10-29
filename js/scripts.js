d0ccabc967ab550138e81e830490ff262d52b30e
gSalvatorec <gsalvadorcr0322@gmail.com>
2020-10-28 22:56:45 -0600

    agregamos validacion de formulario con añadimiento de clases

--- js/scripts.js
+++ js/scripts.js
@@ -23,12 +23,28 @@ function validarInput(e) {
         clase = estado[0];
     }
     // console.log(clase);
+    e.target.classList.remove(...estado);
+    e.target.nextElementSibling.classList.remove(...estado);
+    e.target.classList.add(clase);
+    e.target.nextElementSibling.classList.add(clase);
 
     //Inyectar dinamicamente el div con error
     if (clase === 'no-valido') {
-        //construimos mensaje de error
+        //Validamos que no haya campo de error
+        if (e.target.parentElement.nextElementSibling.classList[0] !== 'alerta') {
+            //construimos mensaje de error
+            const errorDiv = document.createElement('vid');
+            errorDiv.appendChild(document.createTextNode('Este campo es obligatorio'));
+            errorDiv.classList.add('alerta');
+            //insertar el error
+            console.log(e.target);
+            e.target.parentElement.parentElement.insertBefore(errorDiv, e.target.parentElement.nextElementSibling);
+        }
     } else {
         // limpiamos mensaje de error
+        if (e.target.parentElement.nextElementSibling.classList[0] === 'alerta') {
+            e.target.parentElement.nextElementSibling.remove();
+        }
     }
 }
 
